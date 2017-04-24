e17969193e5b237fe40476640a58e79b1ccfdef3
AyresJonas <jayres@restinga.ifrs.edu.br>
2017-04-24 10:24:03 -0300

    projeto inicial

---
+++ Implementacao/WEB/PEDRO/PEDRO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PEDRO.Startup))]
+namespace PEDRO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
