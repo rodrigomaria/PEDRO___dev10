cd10e8e583a82a7629723f8360af4853f9f51fec
rodrigomaria1985 <rammaria@restinga.ifrs.edu.br>
2017-04-27 11:46:26 -0300

    Modificações Models

---
+++ Implementacao/WEB/PEDRO/PEDRO/Models/ArquivosUsuariosModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEDRO.Models
+{
+	public class ArquivosUsuariosModels
+	{
+        [Required]
+        [Display(Name= "Arquivo")]
+        private string nomeDoArquivo {get; set;}
+        [Required]
+        [Display(Name= "Data de upload")]
+        private DateTime dataUpload {get; set;}
+    }
