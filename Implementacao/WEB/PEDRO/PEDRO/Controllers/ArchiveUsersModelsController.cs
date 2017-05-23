8fd64ec08783af94517c7015acabb1025ee21092
AyresJonas <jayres@restinga.ifrs.edu.br>
2017-05-22 21:28:36 -0300

    Ajuste no fluxo, divisão de arquivo por num de nuvens

--- Implementacao/WEB/PEDRO/PEDRO/Controllers/ArchiveUsersModelsController.cs
+++ Implementacao/WEB/PEDRO/PEDRO/Controllers/ArchiveUsersModelsController.cs
@@ -9,6 +9,8 @@ using System.Web.Mvc;
 using PEDRO.Models;
 using Microsoft.AspNet.Identity;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace PEDRO.Controllers
 {
@@ -16,15 +18,13 @@ namespace PEDRO.Controllers
     public class ArchiveUsersModelsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
-        // GET: ArchiveUsersModels
+        
         public ActionResult Index()
         {
             string id = User.Identity.GetUserId();
             return View(db.ArchiveUsersModels.Where(am => am.user.Id == id).ToList());
         }
-
-        // GET: ArchiveUsersModels/Details/5
+        
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -38,22 +38,34 @@ namespace PEDRO.Controllers
             }
             return View(archiveUsersModels);
         }
-
-        // GET: ArchiveUsersModels/Create
+        
         public ActionResult Create()
         {
             return View();
         }
-
-        // POST: ArchiveUsersModels/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(HttpPostedFileBase file)
+        public ActionResult Create(HttpPostedFileBase file, string userKey)
         {
             if (file != null && file.ContentLength > 0 && ModelState.IsValid)
             {
+                if (!VerificaNuvens())
+                {
+                    TempData["Erro"] = "Você deve ter no mínimo um serviço de nuvens cadastrado.";
+                    return RedirectToAction("Erro", "Home");
+                }
+
+                if (userKey.Length < 8 || userKey.Length > 16)
+                {
+                    TempData["Erro"] = "A senha do arquivo deve ter no mínimo 8 e no máximo 16 caracteres.";
+                    return RedirectToAction("Erro", "Home");
+                }
+                else
+                {
+                    for (int i = userKey.Length; i < 16; i++) { userKey = string.Concat(userKey, "0"); }
+                }
+
                 ArchiveUsersModels archiveUsersModels = new ArchiveUsersModels
                 {
                     nomeDoArquivo = file.FileName,
@@ -63,15 +75,33 @@ namespace PEDRO.Controllers
                     user = db.Users.Find(User.Identity.GetUserId())
                 };
 
+                string fileName = Path.GetFileName(file.FileName);
+                string inputFile = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(inputFile);
+
+                try { Encriptar(inputFile, userKey); }
+                catch (Exception ex)
+                {
+                    TempData["Erro"] = "Ocorreu um erro.\nInfo para desenvolvedores: " + ex.HelpLink +
+                        "\n" + ex.Message + "\n" + ex.Data + "\n" + ex.StackTrace;
+
+                    return RedirectToAction("Erro", "Home");
+                }
+
                 db.ArchiveUsersModels.Add(archiveUsersModels);
                 db.SaveChanges();
+
+                Dividir();
+                TempData["Sucesso"] = "Arquivo adicionado com sucesso!";
                 return RedirectToAction("Index");
             }
-
-            return View("Index");
+            else
+            {
+                TempData["Erro"] = "Selecione um arquivo.";
+                return RedirectToAction("Erro", "Home");
+            }
         }
-
-        // GET: ArchiveUsersModels/Edit/5
+        
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -85,10 +115,7 @@ namespace PEDRO.Controllers
             }
             return View(archiveUsersModels);
         }
-
-        // POST: ArchiveUsersModels/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "id")] ArchiveUsersModels archiveUsersModels)
@@ -101,8 +128,7 @@ namespace PEDRO.Controllers
             }
             return View(archiveUsersModels);
         }
-
-        // GET: ArchiveUsersModels/Delete/5
+        
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -116,8 +142,7 @@ namespace PEDRO.Controllers
             }
             return View(archiveUsersModels);
         }
-
-        // POST: ArchiveUsersModels/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -135,6 +160,90 @@ namespace PEDRO.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private void Encriptar(string filePath, string userKey)
+        {
+            var outputFile = Path.Combine(Server.MapPath("~/App_Data/downloads"), "volatil");
+            
+            using (RijndaelManaged aes = new RijndaelManaged())
+            {
+                byte[] key = ASCIIEncoding.UTF8.GetBytes(userKey);
+
+                byte[] IV = ASCIIEncoding.UTF8.GetBytes(userKey);
+
+                using (FileStream fsCrypt = new FileStream(outputFile, FileMode.Create))
+                {
+                    using (ICryptoTransform encryptor = aes.CreateEncryptor(key, IV))
+                    {
+                        using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (FileStream fsIn = new FileStream(filePath, FileMode.Open))
+                            {
+                                int data;
+                                while ((data = fsIn.ReadByte()) != -1)
+                                    cs.WriteByte((byte)data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private int CloudCount()
+        {
+            int c = 0;
+            foreach(var item in db.CloudModels)
+            {
+                if (item.user.Id == User.Identity.GetUserId())
+                    c++;
+            }
+
+            return c;
+        }
+
+        private bool VerificaNuvens()
+        {
+            var nuvens = db.CloudModels.ToList();
+            foreach(var item in nuvens)
+            {
+                if(item.user.Id == User.Identity.GetUserId())
+                    return true;
+            }
+
+            return false;
+        }
+
+        private void Dividir()
+        {
+            int numDeArqs = CloudCount();
+
+            // pasta padrão
+            var path = Path.Combine(Server.MapPath("~/App_Data/downloads"), "volatil");
+
+            //particiona arquivo por partes
+            byte[] todosOsBytes = System.IO.File.ReadAllBytes(path);
+            int x = 0;
+            int fatia = todosOsBytes.Length / numDeArqs;
+            int sobra = todosOsBytes.Length % numDeArqs;
+
+            for (int i = 0; i < numDeArqs; i++)
+            {
+                using (FileStream file = new FileStream(path + "pt" + i, FileMode.Create))
+                {
+                    if (i > 0)
+                    {
+                        file.Write(todosOsBytes, x, fatia);
+                        x += fatia;
+                    }
+                    else
+                    {
+                        file.Write(todosOsBytes, x, fatia + sobra);
+                        x += fatia + sobra;
+                    }
+                }
+
+            }
         }
     }
 }
--- Implementacao/WEB/PEDRO/PEDRO/Controllers/HomeController.cs
+++ Implementacao/WEB/PEDRO/PEDRO/Controllers/HomeController.cs
@@ -18,76 +18,7 @@ namespace PEDRO.Controllers
         {
             return View();
         }
-
-        [Authorize]
-        public ActionResult Encriptar()
-        {
-            return View();
-        }
         
-        [HttpPost]
-        public ActionResult Encriptar(HttpPostedFileBase file, string userKey)
-        {
-            if(userKey.Length < 8 || userKey.Length > 16)
-            {
-                TempData["Erro"] = "A senha do arquivo deve ter no mínimo 8 e no máximo 16 caracteres";
-                return RedirectToAction("Erro");
-            }
-            else
-            {
-                for(int i = userKey.Length; i < 16; i++) { userKey = string.Concat(userKey, "0"); }
-            }
-
-            if (file != null && file.ContentLength > 0)
-            {
-                var fileName = Path.GetFileName(file.FileName);
-                inputFile = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-                file.SaveAs(inputFile);
-                outputFile = Path.Combine(Server.MapPath("~/App_Data/downloads"), "volatil");
-
-                try
-                {
-                    using (RijndaelManaged aes = new RijndaelManaged())
-                    {
-                        byte[] key = ASCIIEncoding.UTF8.GetBytes(userKey);
-
-                        byte[] IV = ASCIIEncoding.UTF8.GetBytes(userKey);
-
-                        using (FileStream fsCrypt = new FileStream(outputFile, FileMode.Create))
-                        {
-                            using (ICryptoTransform encryptor = aes.CreateEncryptor(key, IV))
-                            {
-                                using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
-                                {
-                                    using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
-                                    {
-                                        int data;
-                                        while ((data = fsIn.ReadByte()) != -1)
-                                            cs.WriteByte((byte)data);
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    TempData["Sucesso"] = "Arquivo encriptado com sucesso!";
-                    return RedirectToAction("Dividir");
-                }
-                catch (Exception ex)
-                {
-                    TempData["Erro"] = "Ocorreu um erro.\nInfo para desenvolvedores: " + ex.HelpLink +
-                        "\n" + ex.Message + "\n" + ex.Data + "\n" + ex.StackTrace;
-
-                    return RedirectToAction("Erro");
-                }
-            }
-            else
-            {
-                TempData["Erro"] = "Selecione um arquivo.";
-                return RedirectToAction("Erro");
-            }
-        }
-
         public ActionResult About()
         {
             return View();
@@ -109,53 +40,12 @@ namespace PEDRO.Controllers
 
             return ASCIIEncoding.ASCII.GetString(desCrypto.Key);
         }
-
-        [Authorize]
+        
         public ActionResult Decriptar()
         {
             return View();
         }
-
-        [Authorize]
-        public ActionResult Dividir()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [Authorize]
-        public ActionResult Dividir(int numDeArqs)
-        {
-            // pasta padrão
-            inputFile = Path.Combine(Server.MapPath("~/App_Data/downloads"), "volatil");
-            
-            //particiona arquivo por partes
-            byte[] todosOsBytes = System.IO.File.ReadAllBytes(inputFile);
-            int x = 0;
-            int fatia = todosOsBytes.Length / numDeArqs;
-            int sobra = todosOsBytes.Length % numDeArqs;
-
-            for (int i = 0; i < numDeArqs; i++)
-            {
-                using (FileStream file = new FileStream(inputFile + "pt" + i, FileMode.Create))
-                {
-                    if (i > 0)
-                    {
-                        file.Write(todosOsBytes, x, fatia);
-                        x += fatia;
-                    }
-                    else
-                    {
-                        file.Write(todosOsBytes, x, fatia + sobra);
-                        x += fatia + sobra;
-                    }
-                }
-
-            }
-            
-            return View("Recuperar");
-        }
-
+        
         public ActionResult Recuperar()
         {
             return View();
@@ -175,8 +65,7 @@ namespace PEDRO.Controllers
 
             return View("Decriptar");
         }
-
-        [Authorize]
+        
         [HttpPost]
         public ActionResult Decriptar(HttpPostedFileBase file, string userKey)
         {
