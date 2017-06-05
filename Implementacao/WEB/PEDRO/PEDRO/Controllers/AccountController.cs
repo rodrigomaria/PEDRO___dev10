2b1dc60bba3de893ff4357e25c4455a5eedf1c09
Rodrigo Antônio Menezes Maria <rammaria@restinga.ifrs.edu.br>
2017-06-05 10:28:39 -0300

    removed Minhas Clouds taskbar; after login, redirect to Meus Arquivos; change logo taskbar;

--- Implementacao/WEB/PEDRO/PEDRO/Controllers/AccountController.cs
+++ Implementacao/WEB/PEDRO/PEDRO/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace PEDRO.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "ArchiveUsersModels");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
