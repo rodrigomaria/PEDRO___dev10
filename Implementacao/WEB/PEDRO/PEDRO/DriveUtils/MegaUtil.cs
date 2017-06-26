c5fbcfb5cd3ebde796247aaf2f12f982b666f5d5
andriuswill <awmdsilva@restinga.ifrs.edu.br>
2017-06-26 17:26:57 -0300

    Implementação da API do Mega

---
+++ Implementacao/WEB/PEDRO/PEDRO/DriveUtils/MegaUtil.cs
@@ -0,0 +1,40 @@
+﻿using CG.Web.MegaApiClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEDRO.DriveUtils
+{
+    public class MegaUtil
+    {        
+
+            private String user;
+            private String pass;
+            MegaApiClient client;
+            IEnumerable<INode> nodes;
+            INode root;
+
+            public MegaUtil(string user, string pass)
+            {
+                client = new MegaApiClient();
+                client.Login(
+                    "awmdsilva@restinga.ifrs.edu.br",
+                    "96536976");
+                nodes = client.GetNodes();
+                root = nodes.Single(n => n.Type == NodeType.Root);
+            }
+
+            public void upload(string path, string nome)
+            {
+                client.UploadFile(path, root);
+            }
+
+            public void DeleteFile(string nome)
+            {
+                INode file = nodes.Single(n => n.Name == nome);
+                client.Delete(file, false);
+            }
+        
+    }
+}
